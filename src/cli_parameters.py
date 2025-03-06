0cc995867198d55eb1b7e1f3c0fe5c3a2c877925
Carlos AlMa <carlos@carlosalma.es>
2025-03-06 13:06:42 +0100

    add the first version

---
+++ src/cli_parameters.py
@@ -0,0 +1,19 @@
+"""Gestión de los parámetros del CLI."""
+
+import argparse
+
+def get_cli_parameters(resources):
+    """Gestión de parámetros del CLI."""
+    loc = resources['loc']
+    parser = argparse.ArgumentParser(description=loc['menu']['descrip'])
+
+    parser.add_argument('-p', '--path_repo', type=str,
+                        help=loc['menu']['p'], required=True)
+    parser.add_argument('-f', '--file_out', type=str,
+                        help=loc['menu']['f'], required=True)
+    parser.add_argument('-o', '--salida',
+                        type=str, choices=['csv', 'excel'],
+                        default='csv', required=False, help=loc['menu']['o'])
+
+    args = parser.parse_args()
+    return args.path_repo, args.file_out, args.salida
